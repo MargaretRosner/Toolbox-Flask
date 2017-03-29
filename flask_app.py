72430299390432e671ae39f9b66c023f86b909b1
MargaretRosner <rosner.margaret@gmail.com>
2017-03-28 20:49:35 -0400

    submiting toolbox

--- flask_app.py
+++ flask_app.py
@@ -1,3 +1,28 @@
 """
 Put your Flask app code here.
 """
+
+from flask import Flask
+from flask import render_template
+from flask import request
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['firstname'] and request.form['age'] and request.form['faveninja']:
+            error = None
+            return render_template('login.html', firstname=request.form['firstname'], age=request.form['age'])
+        else:
+            error = 'Invalid username/password'
+            render_template('index.html')
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+if __name__ == '__main__':
+    app.run()
--- hello.py
+++ hello.py
@@ -2,12 +2,23 @@
 Simple "Hello, World" application using Flask
 """
 
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+def hello():
+    return 'Hello World'
+
+@app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
+
+
+
 
 if __name__ == '__main__':
     app.run()
